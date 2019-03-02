a837a8b4120e8f5d41935f6bc9b92a9b816b96f6
Fabian Gutierrez <fega.hg@gmail.com>
2019-03-02 16:56:14 -0500

    Added

--- gitfold.js
+++ gitfold.js
@@ -8,13 +8,45 @@ module.exports = {
       name: 'name',
       message: '¿What is the project name?',
       default: 'node-express-mongodb-skeleton'
+    },
+    {
+      type: 'input',
+      name: 'port',
+      message: '¿What is the default port?',
+      default: 3000
+    },
+    {
+      type: 'input',
+      name: 'expiration',
+      message: '¿JWT token expiration (default 100 days)?',
+      default: '100d'
+    },
+    {
+      type: 'input',
+      name: 'mongodbUri',
+      message: '¿mongodb host?',
+      default: 'mongodb://localhost:27017'
+    },
+    {
+      type: 'input',
+      name: 'dbName',
+      message: '¿database name?',
+      default: 'my-project'
     }
   ],
-  end: config => `
+  end: (config, utils) => {
+    utils.shell.cp('', './.env.example', '.env')
+    utils.shell.rm('', './.env.example')
+
+    const cmd1 = utils.chalk.yellow(`cd ${config.folder}`)
+    const cmd2 = utils.chalk.yellow(`npm start`)
+
+    return `
 DONE!
 next steps:
 
-  cd ${config.folder}
+  ${cmd1}
   
-  npm start`
+  ${cmd2}`
+  }
 }
